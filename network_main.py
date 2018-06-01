ab813e0d22bab83b1c471b1f6a398f2e6625be42
mvonpohle <mvonpohle@gmail.com>
2018-06-01 13:32:20 -0700

    Testing Connection

---
+++ network_main.py
@@ -0,0 +1,24 @@
+import logging
+import time
+
+import network
+
+LOCAL_IP = "192.168.3.10"
+
+'''
+def output:
+    network_test.
+'''
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.debug(network.SERVER_HOST)
+network.SERVER_HOST = LOCAL_IP
+logging.debug(network.SERVER_HOST)
+
+network_test = network.Connection()
+logging.debug(network_test.isConnected())
+#network_test.wait(whenHearCall=output)
+while True:
+    time.sleep(2)
+    logging.debug(network_test.isConnected())
