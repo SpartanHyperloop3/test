15c4a7f1aa8f239105274c599c98029566e7c599
haely <32823897+haely@users.noreply.github.com>
2018-06-22 15:10:20 -0700

    Network watchdog timer (master) thread
    TODO: Test for 2 similar codes running at the same time

---
+++ thread_watchdog.py
@@ -0,0 +1,35 @@
+import threading
+import os
+import time
+
+class NetworkWatchdog(threading.Thread):
+
+    """
+    Pings each of the slave pi's periodically to check for network disconnection
+    In case of a single point of error, emergency command to the slaves is sent.
+    """
+
+    def __init__(self, control):                            #check with Brad about control arg
+        threading.Thread.__init__(self)
+        self._killFlag = threading.Event()
+        self.control = control
+
+    def run(self):
+        ips_list = ["192.168.3.14", "192.168.3.15", "192.168.3.16", "192.168.3.17"]
+        while not self._killFlag.is_set():
+            try:
+                for ip in ips_list:
+                    response = os.system("ping -i 1 -n 1 -l 1 " + ip + " -w 5")
+                    time.sleep(1)
+                    if response == 0:                       #change print function to broadcast state 13
+                        print(ip, 'is up!')
+                    else:                                   #remove else once if function is done
+                        print(ip, 'is down!')
+
+
+#for self-class test, uncomment:
+"""
+    while True:
+        os.system("cls")
+        run()
+"""
